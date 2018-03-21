165c1911fae9114f62d053b7ea8fd7fd0e79ffca
pawhalen <pgwhal@gmail.com>
2018-03-21 18:19:10 -0400

    Getting there

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,42 @@
+var bodyParser = require("body-parser");
+var friendData = require("../data/friends")
+
+module.exports = function(app){
+  //HAVE A LOOK AT hotrestaurant - much different
+    app.get('/api/friends', function(req, res){
+      res.json(friendData);
+    });
+
+    app.post("/api/friends", function(req, res) {
+      var newFriend = req.body
+
+      //Set the best Match to the highest it can be
+      var bestMatch = 50;
+      var bestFriend;   
+    
+      //For each of the friends
+      friendData.forEach(function(friend) {
+        var matchArr = [];
+  
+      //Create a new array representing the distance
+        for (var i = 0; i < 10; i++) {
+          newFriend.scores[i] = parseInt(newFriend.scores[i]);
+          matchArr.push(Math.abs(newFriend.scores[i] - friend.scores[i]));
+        }
+        //Add all the items in new array together
+        var reducer = (a, b) => a + b;
+        var currentMatch = matchArr.reduce(reducer);
+    
+        //if the current match is smaller than the best match this friend is more like the new friend
+        if (currentMatch <= bestMatch) {
+          bestMatch = currentMatch;
+          bestFriend = friend;
+        }
+      });
+      //respond to the POST with the best friend
+      res.json(bestFriend);
+  
+      //after all that add the new friend into the dataset
+      friendData.push(newFriend);
+    });
+  };
